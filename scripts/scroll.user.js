d6bf8f5e5397eb5649416cf62deb2f682ea984b1
Jose Serrano <jsjoseserrano9@gmail.com>
2018-02-22 17:52:43 +0100

    Added keyboard scroll script

---
+++ scripts/scroll.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Keyboard scroll
+// @namespace    https://github.com/just12
+// @version      0.1
+// @author       just12
+// @include      *mangadex.com/chapter/*
+// @include      http://kissmanga.com/Manga/*
+// ==/UserScript==
+window.onkeydown = function(e){
+    var keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed === 87){window.scrollBy(0,-200);}
+    if(keyPressed === 83){window.scrollBy(0,200);}
+};
